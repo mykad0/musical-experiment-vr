ea67afbe3384fd3e74464c7cbf8ab460f7fc00e6
Isaac <isaac.hibou@gmail.com>
2014-12-01 16:53:43 +0100

    Vynil Needle decect nodes
    Colliders & RigidBodies Up to Date

--- src/Assets/Scripts/HighlightOnHover.cs
+++ src/Assets/Scripts/HighlightOnHover.cs
@@ -4,16 +4,26 @@ using System.Collections;
 public class HighlightOnHover : MonoBehaviour {
 
 	public Color startcolor;
-
+	public bool Lock = false;
+	
 	void OnInitialize()
 	{
 		renderer.material.color = startcolor;
 	}
+
 	void OnMouseEnter()
 	{
 		startcolor = renderer.material.color;
 		renderer.material.color = Color.yellow;
 	}
+
+	void OnMouseDown()
+	{
+		renderer.material.color = Color.red;
+		Lock = !Lock;
+		 
+	}
+
 	void OnMouseExit()
 	{
 		renderer.material.color = startcolor;
---
+++ src/Assets/Sequencer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sequencer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("start");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Collision with other GameObjectsCollider
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log(other);
+		renderer.material.color = Color.yellow;	
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		// Debug.Log("Object is within the trigger");
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		// Debug.Log("Object Exited the trigger");
+		renderer.material.color = Color.red;
+	}
+}
