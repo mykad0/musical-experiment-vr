7197b970bc4be79b6eddd999be231109ed382366
Isaac <isaac.hibou@gmail.com>
2014-12-16 23:02:20 +0100

    Circle Visualiser

---
+++ src/Assets/CubeWall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeWall : MonoBehaviour {
+
+	public GameObject cube;
+	public int numberOfObjects = 64;
+	public float radius = 12f;
+
+	// Use this for initialization
+	void Start() {
+
+		int nextNameNumber = 0;
+	    for (int i = 0; i < numberOfObjects; i++) {
+	        float angle = i * Mathf.PI * 2 / numberOfObjects;
+	        Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+	        GameObject cubewow = (GameObject) Instantiate(cube, pos, Quaternion.identity);
+	        cubewow.name = "brick"+nextNameNumber;
+	        nextNameNumber++;
+	    }
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ src/Assets/SpectrumAnalyser.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpectrumAnalyser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float[] spectrum = AudioListener.GetSpectrumData(1024, 0, FFTWindow.Hamming);
+		float c0 = spectrum[3] ;
+		float c1 = spectrum[11] + spectrum[12] + spectrum[13];
+		float c2 = spectrum[22] + spectrum[23] + spectrum[24];
+		float c3 = spectrum[44] + spectrum[45] + spectrum[46] + spectrum[47]  + spectrum[48] + spectrum[49];
+
+
+	 
+
+		GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cubes");
+
+		for( int i=1;i<cubes.Length+1; i=i+3)
+		{
+			 
+		cubes[i].transform.localScale =  new Vector3 (0.5f, 100*(spectrum[i]+spectrum[i+1]+spectrum[i+2]), 0.5f); 
+			cubes[i].transform.position =  new Vector3 (cubes[i].transform.position.x, 50*(spectrum[i]+spectrum[i+1]+spectrum[i+2]), cubes[i].transform.position.z); 
+				 
+		}
+	}
+}
