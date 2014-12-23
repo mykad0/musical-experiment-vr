64f35b1d3d3a54db600ccb98a508716e3ee12d00
Isaac <isaac.hibou@gmail.com>
2014-12-23 16:25:39 +0100

    Correct Merge

--- src/Assets/Scripts/SpectrumAnalyser.cs
+++ src/Assets/Scripts/SpectrumAnalyser.cs
@@ -11,22 +11,39 @@ public class SpectrumAnalyser : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float[] spectrum = AudioListener.GetSpectrumData(1024, 0, FFTWindow.Hamming);
-		float c0 = spectrum[3] ;
-		float c1 = spectrum[11] + spectrum[12] + spectrum[13];
-		float c2 = spectrum[22] + spectrum[23] + spectrum[24];
-		float c3 = spectrum[44] + spectrum[45] + spectrum[46] + spectrum[47]  + spectrum[48] + spectrum[49];
-
-
 	 
-
 		GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cubes");
 
-		for( int i=1;i<cubes.Length+1; i=i+3)
+		for(int i=1;i<cubes.Length;i++)
 		{
-			 
-		cubes[i].transform.localScale =  new Vector3 (0.5f, 100*(spectrum[i]+spectrum[i+1]+spectrum[i+2]), 0.5f); 
-			cubes[i].transform.position =  new Vector3 (cubes[i].transform.position.x, 50*(spectrum[i]+spectrum[i+1]+spectrum[i+2]), cubes[i].transform.position.z); 
-				 
+			if(i < 16)
+			{
+				cubes[i].transform.localScale =  new Vector3 (0.5f, cap(100*(spectrum[i])), 0.5f); 
+				cubes[i].transform.position =  new Vector3 (cubes[i].transform.position.x,  cubes[i].transform.localScale.y / 2, cubes[i].transform.position.z); 
+			}
+			else if(i < 32)
+			{
+				cubes[i].transform.localScale =  new Vector3 (0.5f, cap(100*(spectrum[i] + spectrum[i+1] + spectrum[i+2])), 0.5f); 
+				cubes[i].transform.position =  new Vector3 (cubes[i].transform.position.x,  cubes[i].transform.localScale.y / 2, cubes[i].transform.position.z); 
+			}
+			else if(i < 48)
+			{
+				cubes[i].transform.localScale =  new Vector3 (0.5f, cap(100*(spectrum[i+1] + spectrum[i+2] + spectrum[i+3] + spectrum[i+4] + spectrum[i+5])), 0.5f); 
+				cubes[i].transform.position =  new Vector3 (cubes[i].transform.position.x,  cubes[i].transform.localScale.y / 2, cubes[i].transform.position.z); 
+			}
+			else
+			{
+				cubes[i].transform.localScale =  new Vector3 (0.5f, cap(100*(spectrum[i+1] + spectrum[i+2] + spectrum[i+3] + spectrum[i+4] + spectrum[i+5])), 0.5f); 
+				cubes[i].transform.position =  new Vector3 (cubes[i].transform.position.x,  cubes[i].transform.localScale.y / 2, cubes[i].transform.position.z); 
+			}
 		}
 	}
+
+	float cap( float v) {
+		if(v < 0.1f) { v=0.1f; return v;}
+		else if (v > 20f) { v = 20f; return v;}
+		else { return v; }
+	}
+
+	 
 }
